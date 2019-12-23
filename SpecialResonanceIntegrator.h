f492e92062ca01862fd461c9c55f3481cc9b0bad
xiexinhai <56779005+xiexinhai@users.noreply.github.com>
2019-12-23 20:51:52 +0800

    Create SpecialResonanceIntegrator.h

---
+++ SpecialResonanceIntegrator.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <goofit/GlobalCudaDefines.h>
+#include <goofit/detail/Complex.h>
+
+#include <thrust/functional.h>
+#include <thrust/tuple.h>
+
+namespace GooFit {
+
+class SpecialResonanceIntegrator : public thrust::unary_function<thrust::tuple<int, fptype *, int>, fpcomplex> {
+  public:
+    // Class used to calculate integrals of terms BW_i * BW_j^*.
+    SpecialResonanceIntegrator(int pIdx, unsigned int ri, unsigned int rj);
+    void setDalitzIndex(unsigned int id) { dalitz_i = id; }
+    void setResonanceIndex(unsigned int id) { resonance_i = id; }
+    void setEfficiencyIndex(unsigned int id) { resonance_j = id; }
+    __device__ fpcomplex operator()(thrust::tuple<int, fptype *, int> t) const;
+
+    // Added by xxh for fit fractions
+    void setNoEff() {m_no_eff = true;}
+    void setResonance2Index(unsigned int id) { resonance_j = id; }
+
+
+  private:
+    unsigned int dalitz_i;
+    unsigned int resonance_i;
+    unsigned int resonance_j;
+    unsigned int parameters;
+
+    // Added by xxh for fit fractions
+    bool m_no_eff = false;
+
+};
+
+} // namespace GooFit
