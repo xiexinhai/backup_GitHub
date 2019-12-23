b4c64f1351f2c0570c78c8c1c3708bc71c22aae3
xiexinhai <56779005+xiexinhai@users.noreply.github.com>
2019-12-23 20:51:38 +0800

    Create move_my_files_to_GooFit.sh

---
+++ move_my_files_to_GooFit.sh
@@ -0,0 +1,12 @@
+cp Amp3Body.cu                         ~/GooFit/src/PDFs/physics/Amp3Body.cu
+cp Amp3Body.h                          ~/GooFit/include/goofit/PDFs/physics/Amp3Body.h
+cp FitManagerMinuit2.cpp               ~/GooFit/src/goofit/FitManagerMinuit2.cpp
+cp FitManagerMinuit2.h                 ~/GooFit/include/goofit/fitting/FitManagerMinuit2.h
+cp LASS.cu                             ~/GooFit/src/PDFs/physics/resonances/LASS.cu
+cp MetricTaker.cu                      ~/GooFit/src/PDFs/MetricTaker.cu
+cp Params.cpp                          ~/GooFit/src/goofit/Params.cpp
+cp Params.h                            ~/GooFit/include/goofit/fitting/Params.h
+cp RBW.cu                              ~/GooFit/src/PDFs/physics/resonances/RBW.cu
+cp SpecialResonanceIntegrator.cu       ~/GooFit/src/PDFs/physics/detail/SpecialResonanceIntegrator.cu
+cp SpecialResonanceIntegrator.h        ~/GooFit/include/goofit/PDFs/physics/detail/SpecialResonanceIntegrator.h
+cp dalitz.cpp                          ~/GooFit/examples/dalitz/dalitz.cpp
